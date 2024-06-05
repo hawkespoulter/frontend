bb044909230e231fe7dec817b185bab0a2a50faf
Nathan Geronimo <nathangero@yahoo.com>
2024-06-05 16:10:55 -0700

    implement login persistence

--- src/store/apis/canastaApi.js
+++ src/store/apis/canastaApi.js
@@ -1,18 +1,22 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 import { setToken, clearToken } from "../slices/authSlice";
 
+const KEY_FOR_TOKEN = "canasta_token";
+
 // Define a base query with token handling
 const baseQuery = fetchBaseQuery({
   baseUrl: "http://localhost:3001",
   prepareHeaders: (headers, { getState }) => {
-  const token = getState().auth.token;
-  console.log(token)
-  if (token) {
-    headers.set("authorization", token);
-    console.log(headers)
-  }
-  return headers;
-},
+    // Either get the token from redux store or local storage. Allows for login persistence
+    const token = getState().auth.token || localStorage.getItem(KEY_FOR_TOKEN);
+    // console.log(token);
+
+    if (token) {
+      headers.set("authorization", token);
+      console.log(headers)
+    }
+    return headers;
+  },
 });
 
 const canastaApi = createApi({
@@ -49,8 +53,8 @@ const canastaApi = createApi({
           try {
             const { meta } = await queryFulfilled;
             const token = meta.response.headers.get("authorization");
-            dispatch(setToken( token )); 
-            localStorage.setItem("token", token);
+            dispatch(setToken(token));
+            localStorage.setItem(KEY_FOR_TOKEN, token); // save the token when a page refresh occurs
           } catch {
             // Handle error if needed
           }
@@ -65,6 +69,7 @@ const canastaApi = createApi({
           try {
             await queryFulfilled;
             dispatch(clearToken()); // Clear the token on logout
+            localStorage.removeItem(KEY_FOR_TOKEN); // Remove the now old token from local storage
           } catch {
             // Handle error if needed
           }
